d6a8f6c633ce52a03575d653274678f4a9f0de48
Anderson N. Silva <anddns92@gmail.com>
2023-06-23 05:05:07 -0300

    Criação preliminar do componente de cartão.

---
+++ src/components/DocumentCard.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="max-w-sm bg-gray-800 border border-gray-600 rounded-lg shadow-sm">
+
+        <img src=" ../../template-01.png"
+            class="block object-cover object-top w-full max-w-full p-4 rounded-lg pointer-events-none select-none">
+
+        <div class="px-5 pb-5">
+
+            <h3 class="text-xl font-bold tracking-tight">
+                Lorem Ipsum
+            </h3>
+
+            <span class="text-gray-500">
+                Temporibus dolor
+            </span>
+
+            <p class="mt-3 mb-4 font-light text-gray-500">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+
+            <div class="flex justify-end gap-2 mt-0">
+
+                <button type="button"
+                    class="flex-1 justify-center space-x-2 text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="flex-shrink w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m5.231 13.481L15 17.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v16.5c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9zm3.75 11.625a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
+
+                        <span class="sr-only">Visualizar modelo de currículo</span>
+                    </svg>
+
+                    <span class="block md:hidden lg:block">Visualizar</span>
+
+                </button>
+
+                <button type="button"
+                    class="flex-1 justify-center space-x-2 text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="flex-shrink w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+
+                        <span class="sr-only">Editar modelo de currículo</span>
+                    </svg>
+
+
+                    <span class="block md:hidden lg:block">Editar</span>
+
+                </button>
+
+            </div>
+
+        </div>
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
